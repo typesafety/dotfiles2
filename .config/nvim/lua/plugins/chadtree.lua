78a138504270d4769904b2f085f50ad30571189e
Thomas Li <ailoodee@gmail.com>
2023-04-24 09:06:30 +0200

    Update chadtree.lua

--- .config/nvim/lua/plugins/chadtree.lua
+++ .config/nvim/lua/plugins/chadtree.lua
@@ -1,5 +1,5 @@
 require('chadtree')
 
-vim.keymap.set('n', '<leader>v', '<cmd>CHADopen<cr>')
-vim.keymap.set('n', '<leader>V', '<cmd>CHADopen --always-focus<cr>')
+vim.keymap.set('n', '<C-n>', '<cmd>CHADopen<cr>')
+vim.keymap.set('n', '<leader><C-n>', '<cmd>CHADopen --always-focus<cr>')
 vim.keymap.set('n', '<leader>HH', '<cmd>CHADhelp keybind<cr>')
